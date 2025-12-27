c12d6377debc633ca17692b63c8015087460ae4e
oshikadahal <149761380+oshikadahal@users.noreply.github.com>
2025-12-27 19:40:14 +0545

    Schema.ts code updated

--- app/(auth)/schema.ts
+++ app/(auth)/schema.ts
@@ -1,17 +1,17 @@
 import { z } from "zod";
 
 export const loginSchema = z.object({
-    email: z.string().email({ message: "Enter a valid email" }),
-    password: z.string().min(6, { message: "Minimum 6 characters" }),
+    email: z.string().email({ message: "Enter your valid email" }),
+    password: z.string().min(6, { message: " Must Be Minimum 6 characters" }),
 });
 
 export type LoginData = z.infer<typeof loginSchema>;
 
 export const registerSchema = z.object({
     name: z.string().min(2, { message: "Enter your name" }),
-    email: z.string().email({ message: "Enter a valid email" }),
-    password: z.string().min(6, { message: "Minimum 6 characters" }),
-    confirmPassword: z.string().min(6, { message: "Minimum 6 characters" }),
+    email: z.string().email({ message: "Enter your valid email" }),
+    password: z.string().min(6, { message: "Must Be Minimum 6 characters" }),
+    confirmPassword: z.string().min(6, { message: "Must Be Minimum 6 characters" }),
 }).refine((v) => v.password === v.confirmPassword, {
         path: ["confirmPassword"],
         message: "Passwords do not match",
