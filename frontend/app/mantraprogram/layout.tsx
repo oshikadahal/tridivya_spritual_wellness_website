25a45eef647997bb330a83f2260997022843acc3
oshikadahal <149761380+oshikadahal@users.noreply.github.com>
2026-02-25 20:31:01 +0545

    update 19

--- frontend/app/mantraprogram/layout.tsx
+++
@@ -1,5 +0,0 @@
-import UserLayout from "../(user)/layout";
-
-export default function MantraProgramLayout({ children }: { children: React.ReactNode }) {
-  return <UserLayout>{children}</UserLayout>;
-}
